1a76ed469924b5957b453eabdba9fcb096583599
Migs <mmejares@hotmail.com>
2020-04-21 16:48:43 -0500

    added express.static to render CSS

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../db/db.json");
+var uuid = require("uuid/v1");
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
@@ -6,7 +7,19 @@ module.exports = function(app) {
     });
 
     app.post("/api/notes", function (req, res) {
-        db.push(req.body);
+        let newNote = {
+            id: uuid(),
+            title: req.body.title,
+            text: req.body.text
+        }
+        db.push(newNote);
         res.json(db);
     })
+
+    app.delete("/api/notes/:id", function (req, res) {
+        let id = req.params.id;
+       db = db.filter(note => note.id !== id);
+       res.json(db);
+    })
 };
+
